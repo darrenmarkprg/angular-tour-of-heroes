3efedbd1bc2c32e2e31763b4d46924200c5229c0
darrenmarkprg <85244905+darrenmarkprg@users.noreply.github.com>
2021-06-21 20:08:49 +0000

    Completed part 6 of tutorial

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -31,5 +31,12 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+  }
+
   @Input() hero?: Hero;
 }
